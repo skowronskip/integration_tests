32147b3b6f56d085b6b8efb62c540db13e1b1cc8
Paweł Skowroński <p.skowronski@sharehire.pl>
2019-06-30 15:49:43 +0200

    3.4 LikePostTwice

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikePostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikePostTest.java
@@ -48,4 +48,27 @@ public class LikePostTest extends FunctionalTests {
           .when()
           .post(LIKE_API, 1, 1);
     }
+
+    @Test
+    public void likePostByTwiceShouldNotReturnOK() {
+        RestAssured.given()
+          .accept(ContentType.JSON)
+          .header("Content-Type", "application/json;charset=UTF-8")
+          .expect()
+          .log()
+          .all()
+          .statusCode(HttpStatus.SC_OK)
+          .when()
+          .post(LIKE_API, 3, 1);
+
+        RestAssured.given()
+          .accept(ContentType.JSON)
+          .header("Content-Type", "application/json;charset=UTF-8")
+          .expect()
+          .log()
+          .all()
+          .statusCode(HttpStatus.SC_OK)
+          .when()
+          .post(LIKE_API, 3, 1);
+    }
 }
