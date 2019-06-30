69f3fd19c90f422e5ffb3c23caca88b64e689569
Paweł Skowroński <p.skowronski@sharehire.pl>
2019-06-30 16:08:19 +0200

    3.4 getUsers test

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/FindUsersTest.java
@@ -0,0 +1,30 @@
+package edu.iis.mto.blog.rest.test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.is;
+
+public class FindUsersTest extends FunctionalTests {
+
+    private static final String USER_API = "/blog/user/find";
+
+    @Test
+    public void getShouldReturnUsers() {
+        RestAssured.given()
+                   .accept(ContentType.JSON)
+                   .header("Content-Type", "application/json;charset=UTF-8")
+                   .param("searchString", "John")
+                   .expect()
+                   .log()
+                   .all()
+                   .statusCode(HttpStatus.SC_OK)
+                   .and()
+                   .body("size", is(1))
+                   .when()
+                   .get(USER_API);
+    }
+}
