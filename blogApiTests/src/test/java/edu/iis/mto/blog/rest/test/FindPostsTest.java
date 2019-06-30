e034f2cbf6dd2f49f661806c01646aceb5331de3
Paweł Skowroński <p.skowronski@sharehire.pl>
2019-06-30 16:02:25 +0200

    3.4 getUserPostShouldReturnCorrectLikesCount test

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/FindPostsTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/FindPostsTest.java
@@ -5,6 +5,8 @@ import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.hasItems;
+
 public class FindPostsTest extends FunctionalTests {
 
     private static final String FIND_API = "/blog/user/{id}/post";
@@ -33,5 +35,20 @@ public class FindPostsTest extends FunctionalTests {
                   .statusCode(HttpStatus.SC_BAD_REQUEST)
                   .when()
                   .get(FIND_API, 4);
+    }
+
+    @Test
+    public void getPostOfShouldReturnCorrectLikesCount() {
+        RestAssured.given()
+                  .accept(ContentType.JSON)
+                  .header("Content-Type", "application/json;charset=UTF-8")
+                  .expect()
+                  .log()
+                  .all()
+                  .statusCode(HttpStatus.SC_OK)
+                  .and()
+                  .body("likesCount", hasItems(0))
+                  .when()
+                  .get(FIND_API, 1);
     }
 }
